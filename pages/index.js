c0045e2e29a2b4cd938fd5a52a2556897645c9ad
Cleiton Romualdo de Oliveira Costa <125087714+cleitonromualdo@users.noreply.github.com>
2025-12-13 16:14:07 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-function Home(){
-    return <h1>
-            <p>
-                Esta é um página de teste do curso.dev, 
-                Muito Obrigado a você que acessou ela,
-                Fique a vontande para retornar, se tiver curiosidade
-                e verificar o que está acontecendo por aqui!!!
-            </p>
-        </h1>
+function Home() {
+  return (
+    <h1>
+      <p>
+        Esta é um página de teste do curso.dev, Muito Obrigado a você que
+        acessou ela, Fique a vontande para retornar, se tiver curiosidade e
+        verificar o que está acontecendo por aqui!!!
+      </p>
+    </h1>
+  );
 }
 
 export default Home;
